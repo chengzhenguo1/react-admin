10b0f624b8cb9a66105a8233e06d1ff2ea610fdf
chengzhenguo1 <943944010@qq.com>
2021-04-18 00:11:24 +0800

    feat: 封装less-mixin中的文字省略号

--- src/components/Layout/Header/UserInfo/index.tsx
+++ src/components/Layout/Header/UserInfo/index.tsx
@@ -1,10 +1,11 @@
 import React, { memo } from 'react'
 import { connect } from 'react-redux'
-import { Avatar } from 'antd'
+import { useHistory } from 'react-router-dom'
+import { Avatar, Popconfirm } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
 import { Player } from '@lottiefiles/react-lottie-player'
 import { ClosePath } from '@src/constants/lottiePath'
-import { UserState, logout } from '@src/store/module/user'
+import { logout } from '@src/store/module/user'
 import './index.less'
 import { IStoreState } from '@src/store/type'
 
@@ -13,19 +14,33 @@ interface IProps {
     username: string
 }
 
-const UserInfo: React.FC<IProps> = memo((props) => (
-    <div className='userinfo'>
-        <Avatar size={36} icon={<UserOutlined />} />
-        <h3 className='name'>{props.username}</h3>
-        <div className='close'>
-            <Player
-              autoplay
-              loop
-              src={ClosePath}
-              style={{ height: '58px', width: '58px' }} />
+const UserInfo: React.FC<IProps> = memo((props) => {
+    const { replace } = useHistory()
+    const logOut = () => {
+        props.logout()
+        replace('/')
+    }
+    return (
+        <div className='userinfo'>
+            <Avatar size={36} icon={<UserOutlined />} />
+            <h3 className='name'>{props.username}</h3>
+            <Popconfirm 
+              placement='bottomRight'
+              title='是否登出' 
+              okText='确认' 
+              cancelText='取消'
+              onConfirm={logOut}>
+                <div className='close'>
+                    <Player
+                      autoplay
+                      loop
+                      src={ClosePath}
+                      style={{ height: '58px', width: '58px' }} />
+                </div>
+            </Popconfirm>
         </div>
-    </div>
-))
+)
+ })
 
 export default connect(({ user: { username } }: IStoreState) => ({ username }), {
     logout,
